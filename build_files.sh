7a96488edf878c5dc8d0b3d0d19b63f31205082f
M-Pascal <p.mugisha@alustudent.com>
2024-07-31 00:19:06 +0200

    Deploy

--- build_files.sh
+++ build_files.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# Ensure Python and pip are available
-if ! command -v python3 &>/dev/null; then
-  echo "Python is not installed."
-  exit 1
-fi
-
-if ! command -v pip &>/dev/null; then
-  echo "Pip is not installed."
-  exit 1
-fi
-
 # Set up a virtual environment
 python3 -m venv venv
 source venv/bin/activate
