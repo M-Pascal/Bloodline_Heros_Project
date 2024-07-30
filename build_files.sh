c932c318f8d2bae3f0ef1e6e9d46724835f1f471
M-Pascal <p.mugisha@alustudent.com>
2024-07-31 00:07:30 +0200

    vercel

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-set -e
+python -m venv venv
+
+.\myenv\Scripts\activate
+
 python3.9 -m install -r requirements.txt 
+python3 manage.py migrate
+
 python3.9 manage.py collectstatic --noinput
